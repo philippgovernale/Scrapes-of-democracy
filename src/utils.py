b1d599aea8b9d491a69e181ea7cea15e36236e67
Philipp Governale <philipp.governale1@gmail.com>
2023-11-22 15:40:44 +1300

    improve blurb string cleaning

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
+import re
+
 def get_dmy_str(date_obj) -> str:
 	return date_obj.strftime("%d/%m/%Y")
 
 def clean_str(string: str) -> str:
-	return string.replace('\n','').replace('\t','').replace('\r','').strip()
+	temp1 = re.sub(' +', ' ', string.replace('\t','').replace('\r','').strip())
+	return re.sub('(?<=\n) +','', temp1) #remove leading whitespaces from paragraphs with positive lookbehind
