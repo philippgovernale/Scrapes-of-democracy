9845c8a732607140a3e80d83dde29a191f2f916a
Philipp Governale <philipp.governale1@gmail.com>
2023-12-06 14:10:30 +1300

    Remove duplicate code from hud script

--- src/polis/scripts/hud.py
+++ src/polis/scripts/hud.py
@@ -16,14 +16,12 @@ async def scrape_hud_consultations(get_html_func=_get_soup_urllib) -> list[dict]
 	html_soup = get_html_func(url)
 
 	current = html_soup.find(string='Open activities')
-	print(current.parent.parent.next_sibling)
 
 	#No consultations available
 	if not current:
 		return []
 
 	entries = []
-
 	html_entries = current.parent.parent.parent.find_all('div', {'class': 'col-md-6 col-lg-4'})
 
 	for html_entry in html_entries:
@@ -39,26 +37,5 @@ async def scrape_hud_consultations(get_html_func=_get_soup_urllib) -> list[dict]
 			date_obj = datetime.datetime.strptime(date_unf, "%d %B %Y")
 
 		entries.append(dict(org=polis.SCRAPER_ENUM.hud, title=title, url=url, blurb=blurb, date=date_obj))
-
-	return entries
-
-
-	for descendant in current.parent.parent.parent.ul.descendants:
-		if descendant.name != 'li':
-			continue
-		html_entries.append(descendant)
-
-		for html_entry in html_entries:
-			title =  clean_str(str(html_entry.div.h3.a.string))
-			url = clean_str(str(html_entry.div.h3.a.get('href')))
-			blurb = clean_str(str(html_entry.div.p.string))
-			date_unf = clean_str(str(html_entry.find('p', {'class': 'cs-date-delta'}).text).replace('Closes',''))
-
-			if date_unf.lower() == 'today':
-				date_obj = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
-			else:
-				date_obj = datetime.datetime.strptime(date_unf, "%d %B %Y")
-
-			entries.append(dict(org=polis.SCRAPER_ENUM.hud, title=title, url=url, blurb=blurb, date=date_obj))
 
 	return entries
