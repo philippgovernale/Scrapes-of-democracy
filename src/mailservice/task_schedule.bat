fab1ccdd4a254c0f92bd715ea72bcfd1ba01eb31
Philipp Governale <philipp.governale1@gmail.com>
2024-01-31 22:20:01 +1300

    Make mailservice default run time the same on Windows and Linux

--- src/mailservice/task_schedule.bat
+++ src/mailservice/task_schedule.bat
@@ -1,4 +1,4 @@
 set scriptfolder=%~dp0
 set batfile=run.bat
 set "runbatpath=%scriptfolder%%batfile%"
-schtasks /create /tn "Send Consultations Email" /tr %runbatpath% /sc weekly /d WED /st 13:34
+schtasks /create /tn "Send Consultations Email" /tr %runbatpath% /sc weekly /d WED /st 14:00
